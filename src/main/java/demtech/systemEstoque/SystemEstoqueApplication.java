c6feb03b632a060712d9eac27b2d5d9bbe8efa2a
Matheus Januario <matheusmcj18@hotmail.com>
2023-11-15 19:55:59 -0300

    Criação do projeto systemEstoque, em Java framework Spring Boot

---
+++ src/main/java/demtech/systemEstoque/SystemEstoqueApplication.java
@@ -0,0 +1,13 @@
+package demtech.systemEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demtech/systemEstoque/SystemEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package demtech.systemEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
